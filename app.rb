3b74499fd975d8e95370381c075d55fe2b672e2c
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-11 21:10:25 +0000

    final

--- app.rb
+++ app.rb
@@ -1,9 +1,38 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  result = HTTP.get('https://api.exchangerate.host/symbols')
+
+  parsed_result = JSON.parse(result)
+
+  @curr = parsed_result.fetch('symbols')
+  
+  erb(:first)
+end
+
+get("/:currency") do
+  @selected_curr = params.fetch("currency")
+
+  result = HTTP.get('https://api.exchangerate.host/symbols')
+
+  parsed_result = JSON.parse(result)
+
+  @currencies = parsed_result.fetch('symbols')
+
+  erb(:symbol)
+end
+
+get("/:curr1/:curr2") do
+  @symbol1 = params.fetch("curr1")
+  @symbol2 = params.fetch("curr2")
+  convert_result = HTTP.get("https://api.exchangerate.host/convert?from=#{@symbol1}&to=#{@symbol2}")
+
+  parsed_convert_result = JSON.parse(convert_response)
+
+   @rate = parsed_convert_result.fetch('result')
+
+  erb(:conversion)
 end
